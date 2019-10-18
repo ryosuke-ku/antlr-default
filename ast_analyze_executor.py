c542422d5303bfce8bc260f609389b12465214ea
Kurachi <naist1020@gmail.com>
2019-10-18 19:54:22 +0900

    second commit

--- ast_analyze_executor.py
+++ ast_analyze_executor.py
@@ -4,9 +4,7 @@ from ast.basic_info_listener import BasicInfoListener
 
 
 if __name__ == '__main__':
-    # target_file_path = 'C:/pbl/home/junit/src/test/java/jp/enpit/cloud/junit/CalculatorTest.java'
-    target_file_path = 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven/maven-model-builder/src/test/java/org/apache/maven/model/interpolation/StringSearchModelInterpolatorTest.java'
-    # target_file_path = 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven/maven-model-builder/src/main/java/org/apache/maven/model/interpolation/StringSearchModelInterpolator.java'
+    target_file_path = '解析対象のファイルパス'
 
     # ★ポイント１
     ast_info = AstProcessor(None, BasicInfoListener()).execute(target_file_path)
