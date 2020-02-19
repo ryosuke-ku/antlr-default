aaa76d635db865fa9fda8f25cb0418e8f0a61507
RYOSUKE <kurachi.ryosuke.kp0@is.naist.jp>
2020-02-19 20:00:21 +0900

    first commit

--- ast/ast_processor.py
+++ ast/ast_processor.py
@@ -16,19 +16,5 @@ class AstProcessor:
         parser = JavaParser(CommonTokenStream(JavaLexer(FileStream(input_source, encoding="utf-8"))))
         walker = ParseTreeWalker()
         walker.walk(self.listener, parser.compilationUnit())
-        # self.logger.debug('Display all data extracted by AST. \n' + pformat(self.listener.ast_info, width=160))
-        # print(self.listener.ast_info)
-        # print(self.listener.call_methods)
         print(self.listener.called_methods)
-        print(self.listener.called_methods['setUp'])
-        # print(self.listener.methods['testLocationTrackerShouldBeExcludedFromInterpolation'])
-        # print(len(self.listener.methods['testLocationTrackerShouldBeExcludedFromInterpolation']))
-        print(sum(len(v) for v in self.listener.called_methods['testLocationTrackerShouldBeExcludedFromInterpolation']))
         print(self.listener.methods)
-        for method in self.listener.methods:
-            num = sum(len(v) for v in self.listener.called_methods[method])
-            for i in range(num):
-                print(method)
-                # print(num)
-
-        # return self.listener.ast_info
